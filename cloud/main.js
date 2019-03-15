e2d3bb72d0def7895d8fbf3a018908c25e5de9c8
maks <maksimlya@gmail.com>
2019-03-15 12:22:36 +0200

    Updates

--- cloud/main.js
+++ cloud/main.js
@@ -10,13 +10,39 @@ Parse.Cloud.define("createPoll", async  (request) => {
     let encrypted = security.sign(a.privKey, '0123hello');
 
 
-    let decrypted = security.verifySignature(a.publicKey,encrypted, '0123hello');
+    return security.verifySignature(a.publicKey,encrypted, '0123hello');
 
-    console.log(decrypted);
+});
 
-   // security.GenerateKey('momo');
-   //  security.GenerateKey('moadsasfafsafmo');
-   // security.GenerateKey('mofasfagweweqemo');
-   //  security.GenerateKey('d');
-   // console.log(request.params.pollName, request.params.pollGroup);
-})
+Parse.Cloud.define('createGroup', async (request) => {
+   let groupName = request.params.groupName;
+   const groups = Parse.Object.extend("Groups");
+   const query = new Parse.Query(groups);
+
+    query.equalTo("name", groupName);
+    const results = await query.find();
+
+    if(results.length === 0) {
+        let group = new groups();
+        group.set("name", groupName);
+        group.save();
+    }
+    else return new Parse.Error("Group already exists..");
+});
+
+Parse.Cloud.define('getGroups', async (request) => {
+    let groupsArr = [];
+    const groups = Parse.Object.extend("Groups");
+    const query = new Parse.Query(groups);
+
+    const results = await query.find();
+
+    if(results.length !== 0) {
+        for(let group of results){
+            groupsArr.push(group.get('name'));
+        }
+        return groupsArr;
+    }
+    else return new Parse.Error("No Groups exist....");
+});
+
